83198aa52e288e20e80a7d4854eb4a5d27b8fda1
isakim <bellakm10@gmail.com>
2017-11-03 09:43:49 -0200

    corrigi teste 04

--- src/main.c
+++ src/main.c
@@ -1,17 +1,71 @@
-/* Contador de palavras
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+/*	PrimeSort
  *
- * Este programa recebera uma serie de caracteres representando palavras em sua
- * entrada. Ao receber um caractere fim de linha ('\n'), deve imprimir na tela o
- * numero de palavras separadas que recebeu e, apos, encerrar.
+ *	Esta tarefa é sobre usar pipes e ferramentas do Linux. Nela, você deverá escrever um prorama que 
+ *	recebe como entrada uma sequência de números inteiros positivos, separados por um caractere newline. 
+ *	A sequência é encerrada pelo número -1. O programa deve escrever na tela todos os números não-primos 
+ *	dessa sequência, um por linha ordenados do maior para o menor.
  */
 
 #include <stdio.h>
 
+int ehprimo (int n){
+
+	int div, lim, i;
+
+        if(n==1 || n==0) {
+      		return 0;
+   	} else {
+		div=0;
+       		lim = n+1;
+       		for(i=1; i<lim; i++){
+           		if((n%i)==0){
+               			div++;
+				if (div==3) {
+                                return 0;
+                                }
+                        }
+        	}
+    
+       		if(div==2) {
+          		return 1;
+       		}
+
+        }
+		return 0;
+}
+
 int main() {
 
-  int x, y;
+	int num;
+	FILE *pipe;	
 
-  scanf("%d %d\n", &x, &y);
-  printf("%d\n", x + 200);
-  return 0;
+	if ((pipe = popen("sort -r -n", "w"))==NULL) {
+        	perror("popen");
+    		exit(1);	
+        }
+
+	//scanf("%d", &num);
+
+	while (1){
+
+		scanf ("%d\n", &num);	  	
+
+	  	if (num == -1){	  		
+	  		break;
+	  	} else {
+	  		if (ehprimo(num) == 0){	  			
+	  			fprintf (pipe, "%d\n",num);		  				  				  			
+	  		}
+		}
+	}
+
+	pclose (pipe);
+	  
+	return 0;
 }
